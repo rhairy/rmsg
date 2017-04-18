02e16e3750eec5400bb3f9bb3a6e1c303ed1025b
rhairy <randall.harrison91@gmail.com>
2017-04-17 20:06:48 -0700

    Added getopt

--- rmsg.py
+++ rmsg.py
@@ -1,26 +1,98 @@
 """
 rmsg.py - Send a message of your choice using UDP or TCP.
 """
-
+import getopt
 import socket
 import sys
 
-usage = "rmsg.py <UDP|TCP> <DST.IP.ADDRESS> <DST PORT> <MSG>"
+usage = "rmsg.py --dst <x.x.x.x> --msg <String> --port <0-65536> --proto <tcp|udp"
 
-protocol = sys.argv[1]
-dstipaddress = sys.argv[2]
-dstport = int(sys.argv[3])
-msg = sys.argv[4]
-msglen = len(sys.argv[4])
+# Parse input.
+shortopts = ""
+longopts = ["dst=", "help", "msg=", "port=", "proto="]
+
+dstFlag = False
+dstVal = ""
+
+msgFlag = False
+msgVal = ""
+
+portFlag = False
+portVal = ""
+
+tcpFlag = False
+
+udpFlag = False
+
+
+try:
+	args, left = getopt.getopt(sys.argv[1:],shortopts, longopts)
+except getopt.GetoptError as e: 
+	print(e)
+	exit(-1)
+
+if (len(sys.argv)) < 2:
+	print(usage)
+	exit(-1)
+	
+for opt,val in args:
+	if opt == "--dst":
+		if dstFlag:
+			print(usage)
+			exit(-1)
+		if val:
+			dstFlag = True
+			dstVal = val
+	elif opt == "--help":
+		print(usage)
+		exit(0)
+	elif opt == "--msg":
+		if msgFlag:
+			print(usage)
+			exit(-1)
+		else:
+			msgFlag = True
+			msgVal = val
+	elif opt == "--port":
+		if portFlag:
+			print(usage)
+			exit(-1)
+		else:
+			portFlag = True
+			portVal = val
+	elif opt == "--proto":
+		if tcpFlag or udpFlag:
+			print(usage)
+			exit(-1)
+		else:
+			if val == "tcp":
+				tcpFlag = True
+			elif val == "udp":
+				udpFlag = True
+			else:
+				print(usage)
+				exit(-1)
+	else:
+		print(usage)
+		exit(-1)
+
+if not (dstFlag and msgFlag and portFlag and (udpFlag or tcpFlag)):
+	print(usage)
+	exit(-1)
+
+dstipaddress = dstVal
+dstport = int(portVal)
+msg = msgVal
+msglen = len(msgVal)
 buffer = bytearray([msglen])
 buffer.extend(msg.encode())
 
-if protocol.lower() == "tcp":
+if tcpFlag:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((dstipaddress, dstport))
     s.sendall(buffer)
     s.close()
-elif protocol.lower() == "udp":
+elif udpFlag:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.sendto(buffer, (dstipaddress, dstport))
 else:
--- rmsgd.py
+++ rmsgd.py
@@ -1,16 +1,73 @@
 """
 rmsgd.py - Receive an rmsg using TCP or UDP.
 """
-
+import getopt
 import socket
 import sys
 
-usage = "rmsgd.py <UDP|TCP> <DPORT> <BIND>"
+usage = "rmsgd.py --bind <x.x.x.x> --port <0-65536> --proto <tcp|udp"
+	
+# Parse input.
+shortopts = ""
+longopts = ["bind=", "help", "port=", "proto="]
 
-protocol = sys.argv[1]
-port = int(sys.argv[2])
-bind = '0.0.0.0'
-if (protocol.lower() == "tcp"):
+bindFlag = False
+bindVal = ""
+
+portFlag = False
+portVal = ""
+
+tcpFlag = False
+udpFlag = False
+
+try:
+	args, left = getopt.getopt(sys.argv[1:],shortopts, longopts)
+except getopt.GetoptError as e: 
+	print(e)
+	exit(-1)
+
+for opt,val in args:
+	if opt == "--bind":
+		if bindFlag:
+			print(usage)
+			exit(-1)
+		if val:
+			bindFlag = True
+			bindVal = val
+	elif opt == "--help":
+		print(usage)
+		exit(0)
+	elif opt == "--port":
+		if portFlag:
+			print(usage)
+			exit(-1)
+		else:
+			portFlag = True
+			portVal = val
+	elif opt == "--proto":
+		if tcpFlag or udpFlag:
+			print(usage)
+			exit(-1)
+		else:
+			if val == "tcp":
+				tcpFlag = True
+			elif val == "udp":
+				udpFlag = True
+			else:
+				print(usage)
+				exit(-1)
+	else:
+		print(usage)
+		exit(-1)
+
+if not (bindFlag and portFlag and (udpFlag or tcpFlag)):
+	print(usage)
+	exit(-1)
+
+port = int(portVal)
+bind = bindVal
+
+if tcpFlag:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((bind,port))
 	s.listen()
@@ -20,14 +77,14 @@ if (protocol.lower() == "tcp"):
 		msglen = int.from_bytes(client.recv(1), sys.byteorder)
 		msg = client.recv(int(msglen))
 		print(msg.decode())
-elif (protocol.lower() == "udp"):
+elif udpFlag:
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.bind((bind, port))
 	while True:
 		buffer, clientAddress = s.recvfrom(255)
-		print(clientAddress[0])
 		msglen = buffer[0]
 		msg = buffer[1:msglen+1]
+		print(clientAddress[0])
 		print(msg.decode())
 else:
 	print("Invalid protocol")
